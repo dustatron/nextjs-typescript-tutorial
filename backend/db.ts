259a39e25e99df3863ec1e1209249cb3e9624f74
Dusty McCord <Dustymccord@gmail.com>
2020-11-04 15:56:11 -0800

    add apollo client.

---
+++ backend/db.ts
@@ -0,0 +1,11 @@
+import mysql from "serverless-mysql";
+
+export const db = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
